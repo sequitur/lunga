c8a2f58d1a8160d29d2ec124eb6ff01ed49693a3
Bruno Dias <dev@null.io>
2013-05-18 23:16:05 -0300

    Rewrite completely from scratch.
    With test units this time.

--- lib/lunga.rb
+++ lib/lunga.rb
@@ -1,242 +1,191 @@
 require 'simple-rss'
+require 'singleton'
 require 'open-uri'
 require 'erb'
 require 'time'
 
 class Lunga
 
-  def self.generate
-    # "Main"
-    rcfile = File.join(ENV['HOME'], '.lunga', 'feeds.rb')
+  def self.begin
+    rcfile = File.join ENV['HOME'], '.lunga', 'feeds.rb'
     load rcfile
-
-    template = Templater.new
-
-    spoolfile = File.join(ENV['HOME'], '.lunga', 'spool.html')
-    spool = File.new(spoolfile, 'w')
-    spool.write(template.generate_template)
-
-    # End of normal execution.
-  end
-
-end
-
-def group (name, &proc)
-  # This Kernel method provides the top of the interface to the feeds.rb DSL.
-  Grouper.group_add(name, &proc)
-end
-
-class Grouper
-
-  # Grouper holds all the feed groups as class instance data.
-
-  @groups = []
-
-  def self.group_add (name, &proc)
-    @groups << Group.new(name, &proc)
-  end
-
-  def self.groups
-    @groups
-  end
-
-  def self.items_to_print
-    # Figure out what's to be converted to html.
-    to_print = []
-    puts 'Warning: Nothing to print' unless self.groups
-    self.groups.each do |group|
-      puts 'Warning: No feeds' unless group.feeds
-      puts "Picking feeds for #{group}"
-      group.feeds.each do |f|
-        puts "Getting recent for #{f}"
-        puts 'Warning: Nothing new' unless f.recent
-        to_print << f.recent
-      end
-    end
-    puts "To print: #{to_print}"
-    to_print.flatten.sort do |a, b|
-      b.data.date <=> a.data.date
-    end
-  end
-
-end
-
-class Group
-
-  attr :name, :feeds
-
-  def initialize (name, &proc)
-    @name = name
-    @feeds = []
-    instance_eval(&proc)
-  end
-
-  private
-
-  def feed (name, &proc)
-    @feeds << FeedEntry.new(name, &proc).feed_obj
-  end
-
-end
-
-class MissingAddressError < StandardError; end
-
-class FeedEntry
-
-  # FeedEntry holds the DSL methods that actually define the Feed objects.
-  # It acts as a factory of sorts; each instance is initialized only long
-  # enough to run the DSL code and then promptly spit out the actual Feed.
-
-  attr :feed_obj
-
-  def initialize (name, &proc)
-    @name = name
-    instance_eval(&proc)
-    unless @feed_addr
-      fail MissingAddressError, "No address given for feed #{name}"
-    end
-    @feed_obj = Feed.new(@feed_addr)
-  end
-
-  private
-
-  def cutoff (x)
-    @cutoff = x
-  end
-
-  def feed (feed)
-    @feed_addr = feed
-  end
-
-  def format (format)
-    @format = format
-  end
-
-  def link (link)
-    @link = link
+    ReadingList.instance.populate_feed_list!
+    FeedList.instance.populate_post_list!
+    SpoolWriter.write
   end
 
 end
 
 class Feed
 
-  # Holds the actual feed data.
-  attr :cutoff, :url
+  attr :raw
 
-  def initialize (url, cutoff: (Time.now - 60 * 60 * 24))
-    @url = url
-    @cutoff = cutoff
-  end
-
-  def recent
-    return [] if data == :not_available
-    recent_items = data.items.select do |item|
-      item[:date] && item> @cutoff
-      true
-    end
-
-    recent_items.map do |item|
-      Post.new(item, self)
-    end
-
-  end
-
-  def data
-    if @data
-      @data
-    else
-      fetch!
-      @data
-    end
+  def initialize(rss)
+    @raw = rss
   end
 
   def title
-    # Alias for data nested deep in the rss object model.
-    data.feed.title
+    raw.channel.title
   end
 
-  private
+  def link
+    raw.channel.link
+  end
 
-  def fetch!
-    puts "Fetching #{@url}"
-    open(@url) do |io|
-      @data = SimpleRSS.parse io
+  def items
+    raw.items.map do |item|
+      Post.new(item, self)
     end
-    rescue StandardError => error
-      puts "Cannot open feed: #{error}, skipping"
-      @data = :not_available
   end
 
 end
 
 class Post
 
-  attr :data, :feed
+  attr :channel, :raw_item
 
-  def initialize(data, feed)
-    @data = data
-    @feed = feed
-  end
-
-  def link
-    @data[:link]
-  end
-
-  def channel
-    @feed.title
-  end
-
-  def description
-    text = @data[:content_encoded]
-    text ||= @data[:description]
-
-    # Strip out all <div> tags, so post content can't break formatting. We
-    # wouldn't have to do this if html wasn't permissive.
-    text.gsub(/<\/?div.*>/n, '')
+  def initialize(item, channel)
+    @raw_item = item
+    @channel = channel
   end
 
   def title
-    @data[:title]
+    @raw_item[:title]
   end
 
   def date
-    @data[:date]
+    @raw_item[:pubDate] # rubocop:disable all
+  end
+
+  def link
+    @raw_item[:link]
+  end
+
+  def description
+    text = @raw_item[:content_encoded] || @raw_item[:description]
+    text.gsub(/<\/?div>/, '')
+  end
+
+end
+
+class ReadingList < Array
+  include Singleton
+
+  def populate_feed_list!
+    generate_feeds.each { |feed| FeedList.instance << feed }
+  end
+
+  def generate_feeds
+    map do |feed_entry|
+      feed_entry.generate
+    end
+  end
+
+end
+
+class FeedList < Array
+  include Singleton
+
+  def populate_post_list!
+    generate_posts.each { |post| PostList.instance << post }
+  end
+
+  def generate_posts
+    map do |feed|
+      feed.items
+    end.flatten
+  end
+
+end
+
+class PostList < Array
+  include Singleton
+
+end
+
+class Configuration
+  include Singleton
+
+  attr_accessor :cutoff, :stylesheet, :template, :script, :spool
+
+  def initialize
+    @cutoff = Time.now - 60 * 60 * 24
+    @stylesheet = File.expand_path('lunga/css/style.css',
+                                   File.dirname(__FILE__))
+    @template = File.expand_path('lunga/layout/default.html.erb',
+                                 File.dirname(__FILE__))
+    @script = File.expand_path('lunga/script/lunga.js',
+                                 File.dirname(__FILE__))
+    @spool = File.join ENV['HOME'], '.lunga', 'spool.html'
+  end
+
+end
+
+class ConfigSetter
+
+  def cutoff (value)
+    Configuration.instance.cutoff = Time.now - 60 * 60 * value
+  end
+
+end
+
+class FeedEntry
+  attr :name, :url
+
+  def initialize(name, &proc)
+    @name = name
+    instance_eval(&proc)
+  end
+
+  def channel (url)
+    @url = url
+  end
+
+  def generate
+    rss = SimpleRSS.parse(open(@url))
+    Feed.new(rss)
   end
 
 end
 
 class Templater
 
-  def initialize
+  def initialize (template: Configuration.instance.template)
+    @template_file = template
   end
 
-  def generate_template
-    template_file = File.join(Gem.datadir('lunga'),
-                              'layout',
-                              'default.html.erb')
-    template = File.new(template_file, 'r')
-
+  def spool
+    template = File.new(@template_file, 'r')
     ERB.new(template.read).result binding
   end
 
-  def posts
-    puts "Listing posts..."
-    puts "To print: #{Grouper.items_to_print}"
-    Grouper.items_to_print
-  end
-
   def stylesheet
-    File.join(Gem.datadir('lunga'),
-              'css',
-              'style.css')
+    Configuration.instance.stylesheet
   end
 
   def script
-    File.join(Gem.datadir('lunga'),
-              'script',
-              'lunga.js')
+    Configuration.instance.script
+  end
+
+  def posts
+    PostList.instance
   end
 
 end
 
+class SpoolWriter
 
+  def self.write (spool_file: Configuration.instance.spool)
+    spool = File.new File.expand_path(spool_file), 'w'
+    spool.write Templater.new.spool
+  end
+
+end
+
+def feed (name, &proc)
+  ReadingList.instance << FeedEntry.new(name, &proc)
+end
+
+def settings (&proc)
+  ConfigSetter.new.instance_eval(&proc)
+end
--- spec/lunga_spec.rb
+++ spec/lunga_spec.rb
@@ -1,115 +1,207 @@
 require 'lunga'
-require 'time'
 
-describe Grouper do
+EXAMPLE_FEED = <<EOF
+<?xml version="2.0" encoding="UTF-8"?>
+<channel>
+  <title>My Awesome Feed</title>
+  <link>http://awesome.to</link>
+  <item>
+    <title>My Sweet Post</title>
+    <link>http://awesome.to/sweet</link>
+    <pubDate>Sat, 18 may 2013 16:00:00 +0000</pubDate>
+    <description>Awesome, relevant post content.</description>
+    <content:encoded>
+      <![CDATA[<p>Awesome, relevant post content - in a paragraph!<p>]]>
+    </content:encoded>
+  </item>
+</channel>
+EOF
 
-  context 'DSL' do
+MALFORMED_FEED = <<EOF
+<?xml version="2.0" encoding="UTF-8"?>
+<channel>
+  <title>My Awesome Feed</title>
+  <link>http://awesome.to</link>
+  <item>
+    <title>My Sweet Post</title>
+    <link>http://awesome.to/sweet</link>
+    <pubDate>Sat, 18 may 2013 16:00:00 +0000</pubDate>
+    <description>Awesome, relevant post content.</description>
+    <content:encoded>
+      <![CDATA[<div>
+      <p>Awesome, relevant post content - in a paragraph!<p></div>]]>
+    </content:encoded>
+  </item>
+</channel>
+EOF
 
-    group 'my group' do
+CUTOFF_DEFAULT = Time.now - 60 * 60 * 24
+CSS_DEFAULT = File.expand_path('../lib/lunga/css/style.css',
+                               File.dirname(__FILE__))
+TEMPLATE_DEFAULT = File.expand_path('../lib/lunga/layout/default.html.erb',
+                                    File.dirname(__FILE__))
 
-      feed 'my feed' do
-        feed 'http://myaddre.ss'
-      end
-
-    end
-
-    group = Grouper.groups[-1]
-
-    it 'takes a DSL and spits out Group objects' do
-
-      group.should be_a_kind_of(Group)
-      group.name.should eq('my group')
-
-    end
-
-    it 'creates Group objects that hold feeds' do
-
-      group.feeds[0].should be_a_kind_of(Feed)
-      group.feeds[0].url.should eq('http://myaddre.ss')
-
-    end
-
-  end
-
-  it 'raises an error if the DSL is invalid' do
-    expect do
-
-      group 'wrong group' do
-        feed 'wrong feed' do
-        end
-      end
-
-    end.to raise_error(MissingAddressError)
-
-  end
-
-end
-
-describe Group do
-
-end
-
-describe Post do
-
-  it 'holds post and feed data' do
-    date = Date.parse('2013-05-18')
-    rss = { content_encoded: '<p>Hello!</p>',
-            link:            'http://link.to',
-            date:            date}
-
-    feed = double('feed')
-    feed.stub(:title => 'My brilliant feed')
-
-    post = Post.new(rss, feed)
-
-    post.link.should eq('http://link.to')
-    post.description.should eq('<p>Hello!</p>')
-    post.date.should eq(date)
-    post.channel.should eq('My brilliant feed')
-  end
-
-end
-
-shared_context 'after fetching a feed' do |feed|
-
-  it 'fetches data if it has not already' do
-    feed.should_receive(:fetch!).and_call_original
-    feed.data
-  end
-
-  it 'Holds some sort of data object' do
-    data = feed.data
-    data.should be_a_kind_of(SimpleRSS)
-  end
-
-  it 'has a title method that gets its title from the channel data' do
-    feed.title.should be_a_kind_of(String)
-  end
-
-  it 'holds a list of recent post objects, which may be empty' do
-    feed.recent.should be_a_kind_of(Array)
-  end
-
+shared_context 'rss' do
+  let(:feed) { Feed.new(SimpleRSS.parse EXAMPLE_FEED) }
+  let(:post) { feed.items[0] }
 end
 
 describe Feed do
+  context 'with a valid feed' do
+    include_context 'rss'
 
-  it 'has a default cutoff' do
-    feed = Feed.new('http://xkcd.com/rss')
+    it 'wraps a raw SimpleRSS object' do
+      feed.raw.should be_a_kind_of SimpleRSS
+    end
 
-    # Ten minutes is enough time to run the test suite, surely?
-    feed.cutoff.should be_within(600).of( Time.now - 60 * 60 * 24 )
+    it 'provides convenience methods for channel data' do
+      feed.title.should eq 'My Awesome Feed'
+      feed.link.should eq 'http://awesome.to'
+    end
+
+    it 'generates a list of feed items' do
+      feed.items.should be_a_kind_of Array
+      feed.items[0].should be_a_kind_of Post
+    end
+  end
+end
+
+describe Post do
+  context 'rss feed' do
+    include_context 'rss'
+
+    it 'knows about the feed object that spawned it' do
+      post.channel.should eq feed
+    end
+
+    it 'contains a raw hash of post attributes' do
+      post.raw_item.should be_a_kind_of Hash
+    end
+
+    it 'has convenience methods for post data' do
+      post.title.should eq 'My Sweet Post'
+      post.date.should eq Time.parse('Sat, 18 may 2013 16:00:00 +0000')
+    end
+
   end
 
-  context 'using rss' do
-    feed = Feed.new('http://xkcd.com/rss.xml')
-    include_examples 'after fetching a feed', feed
-  end
+  context 'bad feeds' do
+    feed = Feed.new(SimpleRSS.parse(MALFORMED_FEED))
+    post = feed.items[0]
 
-  context 'using atom' do
-    feed = Feed.new('http://xkcd.com/atom.xml')
-    include_examples 'after fetching a feed', feed
+    it 'strips out all divs' do
+      post.description.should_not =~ /<div>/
+      post.description.should_not =~ /<\/div>/
+    end
   end
 
 end
 
+describe FeedEntry do
+  context 'adding new entries to ReadingList with Kernel#feed' do
+    # We take advantage, here, of the fact that open doesn't care whether it
+    # gets a local filename or a remote url.
+    feed 'Example Feed' do
+      channel 'spec/data/examplefeed.xml'
+    end
+
+    it 'holds a name and URL to the feed itself' do
+      entry = ReadingList.instance[-1]
+      entry.should be_a_kind_of FeedEntry
+      entry.name.should eq 'Example Feed'
+      entry.url.should eq 'spec/data/examplefeed.xml'
+    end
+
+    it 'can create a real, faithful Feed object from its data' do
+      entry = FeedEntry.new('Example Feed') do
+        channel 'spec/data/examplefeed.xml'
+      end
+
+      feed = entry.generate
+      feed.should be_a_kind_of Feed
+      feed.title.should eq 'Balaclava Fashion'
+    end
+
+  end
+
+end
+
+describe ReadingList do
+  it 'is a singleton' do
+    a, b = ReadingList.instance, ReadingList.instance
+    a.should eq b
+  end
+
+  it 'can produce an array of Feed objects' do
+    feed_list = ReadingList.instance.generate_feeds
+    feed_list.should be_a_kind_of Array
+  end
+
+end
+
+describe FeedList do
+  it 'is a singleton array of Feed objects taken from ReadingList' do
+    a, b = FeedList.instance, FeedList.instance
+    a.should eq b
+    ReadingList.instance.populate_feed_list!
+    FeedList.instance[0].should be_a Feed
+  end
+end
+
+describe PostList do
+  it 'is a flat, singleton array of Post objects taken from FeedList' do
+    a, b = PostList.instance, PostList.instance
+    a.should eq b
+    FeedList.instance.populate_post_list!
+    PostList.instance.should be_a Array
+    PostList.instance[0].should be_a Post
+  end
+
+end
+
+describe Configuration do
+  it 'holds configuration options, with defaults' do
+    config = Configuration.instance
+    config.should eq Configuration.instance
+    config.cutoff.should be_within(600).of(CUTOFF_DEFAULT)
+    config.stylesheet.should eq (CSS_DEFAULT)
+    config.template.should eq (TEMPLATE_DEFAULT)
+  end
+
+  it 'can be configured with Kernel#settings' do
+    new_cutoff = Time.now - 60 * 60 * 48
+
+    settings do
+      cutoff 48 # 48 hours
+    end
+
+    Configuration.instance.cutoff.should be_within(600).of new_cutoff
+
+  end
+
+end
+
+describe Templater do
+  context 'with a dummy template file' do
+    templater = Templater.new(template: 'spec/data/exampletemplate.html.erb')
+
+    it 'generates a spool file from PostList' do
+      spool = templater.spool
+      spool.should be_a String
+      spool.should =~ /<!doctype html>/
+    end
+
+  end
+
+  context 'with a real template file' do
+    templater = Templater.new
+
+    it 'generates a spool file from PostList' do
+      spool = templater.spool
+      spool.should be_a String
+      spool.should =~ /<!doctype html>/
+      spool.should =~ /Balaclava balaclava balaclava\?/
+    end
+  end
+
+end
